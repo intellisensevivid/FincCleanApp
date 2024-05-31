85422a3d9066c3b1de1717c59c2b22523e754fa6
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2024-05-31 15:50:02 +0100

    Completed service api

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,5 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode ? err.statusCode : 500;
+  // console.error(err)
   res.status(statusCode).json({
     error: err.message,
     statusCode,
---
+++ backend/util/storage.js
@@ -0,0 +1,21 @@
+const multer = require("multer");
+const { v2: cloudinary } = require("cloudinary");
+const { config } = require("dotenv");
+config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = multer.diskStorage({
+  destination: "./uploads",
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + file.originalname);
+  },
+});
+
+const uploader = multer({ storage });
+
+module.exports = uploader;
